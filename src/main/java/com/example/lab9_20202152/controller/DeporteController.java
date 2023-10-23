e723ca97e27b8504df2a3d881106ba45b8869a35
AnaisSalvador <anaissalvadorrodriguez@gmail.com>
2023-10-22 23:02:24 -0500

    deporte commit

--- src/main/java/com/example/lab9_20202152/controller/DeporteController.java
+++ src/main/java/com/example/lab9_20202152/controller/DeporteController.java
@@ -3,12 +3,14 @@ import com.example.lab9_20202152.entity.Deporte;
 import com.example.lab9_20202152.repository.DeporteRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/deporte")
@@ -41,6 +43,39 @@ public class DeporteController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
     }
 
+    //actualizar
+    @PutMapping(value = {"", "/"}, consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    public ResponseEntity<HashMap<String, Object>> actualizar(Deporte nombreRecibio) {
+
+        HashMap<String, Object> rpta = new HashMap<>();
+
+        if (nombreRecibio.getId() != null && nombreRecibio.getId() > 0) {
+
+            Optional<Deporte> byId = deporteRepository.findById(nombreRecibio.getId());
+            if (byId.isPresent()) {
+                Deporte productFromDb = byId.get();
+
+                if (nombreRecibio.getNombreDeporte() != null)
+                    productFromDb.setNombreDeporte(nombreRecibio.getNombreDeporte());
+
+                if (nombreRecibio.getPesoDeporte() != null)
+                    productFromDb.setPesoDeporte(nombreRecibio.getPesoDeporte());
+
+
+                deporteRepository.save(productFromDb);
+                rpta.put("resultado", "ok");
+                return ResponseEntity.ok(rpta);
+            } else {
+                rpta.put("resultado", "error");
+                rpta.put("msg", "El ID escrito no existe");
+                return ResponseEntity.badRequest().body(rpta);
+            }
+        } else {
+            rpta.put("resultado", "error");
+            rpta.put("msg", "Enviar correctamente los datos");
+            return ResponseEntity.badRequest().body(rpta);
+        }
+    }
 
 
 }
